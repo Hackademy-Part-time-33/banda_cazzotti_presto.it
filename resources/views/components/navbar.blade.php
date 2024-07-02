8499de920d9f78ad54d01e132bd26f18d6261a0e
MarcoPanz <marcopanzan23@gmail.com>
2024-07-02 20:24:37 +0200

    modifica navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,34 +8,38 @@
                 loading="lazy" style="margin-top: -1px;" />
         </a>
 
-        <!-- Toggle button -->
-        <button class="navbar-toggler" type="button">
-            <i class="fas fa-bars"></i>
-        </button>
+
 
         <!-- Collapsible wrapper -->
-        <div class="collapse navbar-collapse" id="navbarButtonsExample">
-            <!-- Left links -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home casa </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Chi siamo</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#">
-                        Lista prodotti
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-            </ul>
-            <!-- Left links -->
 
-            <div class="d-flex align-items-center">
+        <!-- Left links -->
+
+        <div class="d-flex align-items-end">
+            <div class="collapse navbar-collapse" id="navbarButtonsExample">
+                <!-- Left links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home </a>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+
                 <button type="button" class="btn btn-link px-3 me-2">
                     Login
                 </button>
