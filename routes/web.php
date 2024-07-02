eeec05d6c813ac24ac2f57d300162a4b0d470374
Marco De Vito <marco.devito93@gmail.com>
2024-07-02 20:51:19 +0200

    in web.php articles con la A minuscola

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ Route::get('/', [PageController::class,'homepage'])->name('homepage');
 Route::get('/chi-siamo', [PageController::class,'about'])->name('about');
 
 Route::get('/contatti', [PageController::class,'contacts'])->name('contacts');
-Route::resource('Articles', ArticleController::class);
+Route::resource('articles', ArticleController::class);
