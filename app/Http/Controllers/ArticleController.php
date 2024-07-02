7a12a35eaa8b7f51aaf41f8c7151e8e82782f2b7
Marco De Vito <marco.devito93@gmail.com>
2024-07-02 20:36:15 +0200

    added middleware in articlecontroller only create

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Http\Requests\StoreArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class ArticleController extends Controller
+class ArticleController extends Controller implements HasMiddleware
 {
     /**
      * Display a listing of the resource.
      */
+    public static function middleware():array
+    {
+        return [new Middleware('auth', only: ['create'])];
+    } 
     public function index()
     {
         $articles= Article::orderBy('created_at', 'desc')->paginate(5);
@@ -22,7 +28,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('article.create');
     }
 
     /**
@@ -30,7 +36,7 @@ class ArticleController extends Controller
      */
     public function store(StoreArticleRequest $request)
     {
-        //
+        
     }
 
     /**
