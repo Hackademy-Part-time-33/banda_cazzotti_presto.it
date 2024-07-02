4f5b891ee0c3e7f1df62ba0a91c95e014530cc1d
Elia Armenti <armentielia@gmail.com>
2024-07-02 20:42:24 +0200

    aggiunte relazioni hasmany e belongsto ai model user e category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable =['name'];
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class);
+    }
 }
