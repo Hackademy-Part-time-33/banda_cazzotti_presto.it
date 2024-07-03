a44b0a4a9af442b7ac0b6b04b08bf906008b29b8
lauravitali1978 <laura@gaw.agency>
2024-07-03 19:32:51 +0200

    creazione view index di articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,19 +7,23 @@
                 <th>Nome</th>
                 <th>Descrizione</th>
                 <th>Prezzo</th>
-                
+                <th>Aggiunto il</th>
+
             </tr>
         </thead>
         <tbody>
             @forelse($articles as $article)
             <tr>
-                <td>{{ $article->name }}</td>
+                <td>{{ $article->title }}</td>
                 <td>{{ $article->description }}</td>
                 <td>{{ $article->price }}</td>
+                <td>{{ $article->created_at }}</td>
+
             </tr>
             @empty
             @endforelse
         </tbody>
     </table>
+    {{ $articles->links() }}
 </div>
 </x-main>
