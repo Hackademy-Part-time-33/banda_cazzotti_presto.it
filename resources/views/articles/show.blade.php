d7a856c3657125282d8c8393f04441ededf35255
agostino <agostinocuomo9@gmail.com>
2024-07-03 21:23:41 +0200

    show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,32 +1,43 @@
+<head>
+    @vite(['resources/css/show.css', 'resources/js/show.js'])
+    <head>
+
 <x-main>
 
   {{--   CAROSELLO INIZIALE --}}
+<div class="d-flex flex-sm-column flex-lg-row">
+
 
   <div id="carouselExampleIndicators" class="carousel carousel-dark slide border border-5 border-warning ">
     <div class="carousel-indicators">
-        <h1>Nome dell'articolo: {{$article->title}}</h1>
+      
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
 
+
+
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="LEGO.jfif" class="d-block w-100  p-5" id="img-carousel" alt="img-1">
-
-      </div>
-      <div class="carousel-item">
-        <img src="LEGO2.jfif" class="d-block w-100  p-5" id="img-carousel"  alt="img-2">
-      </div>
-      <div class="carousel-item">
-        <img src="lego3.jfif" class="d-block w-100  p-5" id="img-carousel"  alt="img-3">
-      </div>
+        <img src="{{ asset('LEGO2.jfif') }}" class="d-block w-100  p-5" id="img-carousel" alt="img-1">
+    
     </div>
 
-    <h3>Titolo: {{$article->title}}</h3>
-    <h4 class="fw-bold">Prezzo: {{$article->price}} €</h4>
-    <h5>Descrizione</h5>
-    <p>{{$article->description}}</p>
+      <div class="carousel-item">
+        <img src="{{ asset('LEGO.jfif') }}" class="d-block w-100  p-5" id="img-carousel"  alt="img-2">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>{{$article->title}}</h5>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="{{ asset('lego3.jfif') }}" class="d-block w-100  p-5" id="img-carousel"  alt="img-3">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>{{$article->title}}</h5>
+        </div>
+      </div>
+    </div>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -37,5 +48,11 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-
+<div class="d-flex flex-column m-5">
+  <h3>Titolo: {{$article->title}}</h3>
+  <h4 class="fw-bold">Prezzo: {{$article->price}} €</h4>
+  <h5>Descrizione</h5>
+  <p>{{$article->description}}</p>
+</div>
+</div>
 </x-main>
