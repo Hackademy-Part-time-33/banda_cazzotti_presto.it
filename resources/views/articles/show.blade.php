d520fe8df0551c748a3a4d594effdf47eabf1dc6
Elia Armenti <armentielia@gmail.com>
2024-07-03 19:38:12 +0200

    aggiunta carosello per visualizzare i dettagli del prodotto

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,10 +1,10 @@
 <x-main>
 
   {{--   CAROSELLO INIZIALE --}}
-    
 
   <div id="carouselExampleIndicators" class="carousel carousel-dark slide border border-5 border-warning ">
     <div class="carousel-indicators">
+        <h1>Nome dell'articolo: {{$article->title}}</h1>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -22,6 +22,10 @@
       </div>
     </div>
 
+    <h3>Titolo: {{$article->title}}</h3>
+    <h4 class="fw-bold">Prezzo: {{$article->price}} €</h4>
+    <h5>Descrizione</h5>
+    <p>{{$article->description}}</p>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
