b3d1c498313b3ca157531b3ca3e03f46d64bd56d
Marco De Vito <marco.devito93@gmail.com>
2024-07-04 19:39:13 +0200

    colonna autori aggiunta in index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -9,6 +9,7 @@
                     <th>Prezzo</th>
                     <th>Categoria</th>
                     <th>Aggiunto il</th>
+                    <th>Autore</th>
                     <th>Azioni</th>              
                 </tr>
             </thead>
@@ -23,6 +24,7 @@
                     <td>{{ "€".Str::replace('.', ',', sprintf("%.2f", $article->price))}}</td>
                     <td><a href="{{route('byCategory', ['category'=>$article->category])}}">{{ $article->category->name }}</a></td>
                     <td>{{ $article->created_at->locale(app()->getLocale())->translatedFormat('d F Y, H:i') }}</td>  
+                    <td>{{$article->user->name}}</td>
                     <td>
                         <a href="{{ route('articles.show', $article->id) }}" class="btn btn-primary">Visualizza</a>
                         {{-- <a href="{{ route('articles.edit', $article->id) }}" class="btn btn-warning">Modifica</a>
