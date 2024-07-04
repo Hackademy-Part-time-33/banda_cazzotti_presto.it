9a1ed99baff57ec5e733d594c6e395459299653f
Marco De Vito <marco.devito93@gmail.com>
2024-07-04 16:54:16 +0200

    sistemato factory di article, ora le descrizioni sono più lunghe

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -20,8 +20,8 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => Str::ucfirst(fake()->word(4)),
-            'description' =>fake()->words(5,true),
+            'title' => Str::ucfirst(fake()->words(rand(1,5),true)),
+            'description' =>fake()->words(rand(24,50),true),
             'price' => fake()->randomFloat(2, 9, 500),
             'category_id' => rand(1,Category::all()->count()),
             'user_id' => User::factory() ,
