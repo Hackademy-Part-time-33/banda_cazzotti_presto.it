62ebf7b1ec2e44ec8f356c9e90f84145a0d35eac
Marco De Vito <marco.devito93@gmail.com>
2024-07-08 19:11:09 +0200

    aggiornato composer e installato scout aggiunta funzione in article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Searchable;
 
 class Article extends Model
 {
+    use Searchable;
     use HasFactory;
     protected $fillable =['title', 'description','price','category_id','user_id'];
 
@@ -26,5 +28,14 @@ class Article extends Model
         $this->save();
         return true;
     }
+    public function toSearchableArray() {
+        return [
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'description'=>$this->description,
+            'category'=>$this->category,
+        ];
+    }
+
 }
 
