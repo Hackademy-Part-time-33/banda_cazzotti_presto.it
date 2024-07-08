d29e603d8e5dcb578980f07ff6f42ddda083369c
Elia Armenti <armentielia@gmail.com>
2024-07-08 20:09:45 +0200

    aggiunta vista become-revisor

---
+++ app/Mail/BecomeRevisor.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class BecomeRevisor extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Rendi Revisore il richiedente' . $this->user->name,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.become-revisor',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
