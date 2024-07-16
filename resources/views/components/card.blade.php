fb162258c1dcc6ae1109b6403bc9f797c1753272
Marco De Vito <marco.devito93@gmail.com>
2024-07-16 12:53:44 +0200

    logica card blade sistemate

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="card mx-auto shadow text-center {{-- m-4 --}}" style="width: {{$width ?? 20}}rem; height : {{$height ?? 400}}px">
-        <img src="{{$article->images->isNotEmpty()? Storage::url($article->images->first()->path):asset("LogoLegoNavbar.png")}}" class="card-img-top p-3" alt="Immagine dell'articolo {{$article->title}}" >
+        <img src="{{$article->images->isNotEmpty()? Storage::url($article->images->first()->path):asset("LogoLegoNavbar.png")}}" class="card-img-top @if(!$article->images->isNotEmpty()) bg-danger p-3 @else  @endif" alt="Immagine dell'articolo {{$article->title}}" >
         <div class="card-body">
-            <h4 class="card-title">{{$article->title}}</h4>
+            <h4 class="card-title">{{Str::limit($article->title,15) }}</h4>
             <h6 class="card-subtitle ">{{ Str::replace('.', ',', sprintf("%.2f", $article->price))}} €</h6>
-            <div class="d-flex justify-content-evenly align-items-center mt-5">
+            <div class="d-flex justify-content-evenly align-items-center mt-4">
                 <a href="{{route('articles.show', compact('article'))}}" class="btn custom-button me-2">Dettaglio</a>
                 <a href="{{route('byCategory', ['category'=>$article->category])}}" class="btn custom-button ">{{$article->category->name}}</a>
             </div>
