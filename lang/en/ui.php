dbbbedcb9d2f9ad2f6b0b220a4eb2110984bfa7a
lauravitali1978 <laura@gaw.agency>
2024-07-17 17:19:44 +0200

    tradotta navbar e titolo pagine nascoste in lingua inglese e spagnola

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -1,6 +1,21 @@
 <?php
 return[
     'hello' => 'Hello',
-    'allArticles' => 'All the articles'
-    
+    'allArticles' => 'All articles',
+    'home'=> 'Homepage',
+    'no_categories'=> 'No categories available',
+    'description'=>'Description',
+    'price'=>'Price',
+    'category'=>'Category',
+    'categories'=>'Categories',
+    'about_us' => 'About us',
+    'contacts'=>'Contacts',
+    'products'=>'All products',
+    'login'=>'Login',
+    'register'=>'Register',
+    'preferites'=>'this week s preferites', 
+    'work_with_us'=> 'Work with us',
+    'insert_ad'=> 'Insert ad',
+    'revisor_area'=> 'Revisor area'
+
 ];
--- lang/it/ui.php
+++ lang/it/ui.php
@@ -2,5 +2,20 @@
 return[
 
     'hello' => 'Ciao',
-    'allArticles' => 'Tutti gli articoli'
+    'allArticles' => 'Tutti gli articoli',
+    'home'=> 'Home',
+    'no_categories'=> 'Nessuna categoria',
+    'description'=>'Descrizione',
+    'price'=>'Prezzo',
+    'category'=>'Categoria',
+    'categories'=>'Categorie',
+    'contacts'=>'Contatti',
+    'about_us'=> 'Chi siamo',
+    'products'=>'Tutti i prodotti',
+    'login'=>'Accedi',
+    'register'=>'Registrati',
+    'preferites'=>'I preferiti di questa settimana',
+    'work_with_us'=> 'Lavora con noi',
+    'insert_ad'=> 'Inserisci annuncio',
+    'reviser_area'=> 'Area riservata'
 ];
