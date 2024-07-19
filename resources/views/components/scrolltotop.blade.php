7edaf4d9052d7010b6fafbed3e67a92428a2c636
Marco De Vito <marco.devito93@gmail.com>
2024-07-19 20:47:03 +0200

    risolto problema con scrolltop e componente

--- resources/views/components/scrolltotop.blade.php
+++ resources/views/components/scrolltotop.blade.php
@@ -1,5 +1,13 @@
-
-
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-
-
+<div>
+    
+    
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        
+    <script>
+        function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+    </script>
+    
+</div>
