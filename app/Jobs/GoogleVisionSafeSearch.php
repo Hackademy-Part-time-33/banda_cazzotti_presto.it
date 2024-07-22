034ee74998b70eb6e832da166ded530067a5e621
Marco De Vito <marco.devito93@gmail.com>
2024-07-22 20:02:36 +0200

    aggiunto nuovo job googlevisionlabelimage

--- app/Jobs/GoogleVisionSafeSearch.php
+++ app/Jobs/GoogleVisionSafeSearch.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\Image;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
