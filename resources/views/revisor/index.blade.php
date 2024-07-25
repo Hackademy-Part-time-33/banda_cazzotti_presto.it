1f1777d7ceaac1d7717f39d180ae35ee483ad1d9
Marco De Vito <marco.devito93@gmail.com>
2024-07-25 18:33:06 +0200

    invertiti pulsanti nel revisor index

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -149,17 +149,18 @@
                 </div>
                 
                 <div class="d-flex pb-4 justify-cointent-around ">
+                     <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="post">
+                        @csrf
+                        @method('PATCH')
+                        <button class="btn btn-danger py-2 px-5 fw-bold m-2">Accetta</button>
+                    </form>
                     <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="post">
                         @csrf
                         @method('PATCH')
                         <button class="btn btn-danger py-2 px-5 fw-bold m-2">Rifiuta</button>
                     </form>
                     
-                    <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="post">
-                        @csrf
-                        @method('PATCH')
-                        <button class="btn btn-danger py-2 px-5 fw-bold m-2">Accetta</button>
-                    </form>
+                   
                 </div>
             </div>
         </div>
